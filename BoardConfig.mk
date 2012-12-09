07d47c14f0d9f27b1c6e32aa1d021d8e1ed99bf1
Joey Troy <joey@joeytroy.com>
2012-12-09 00:46:11 -0700

    Merged htc_msm8660
    Merged in all the settings from htc_msm8660-common as to keep the devices all in one folder.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,33 +1,143 @@
+# Copyright (C) 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+
+##########################################################
+#  These are Architecture settings for the HTC Evo V 4G  #
+#          				                 #
+##########################################################
+
+# This variable is set first, so it can be overridden
+# by BoardConfigVendor.mk
 USE_CAMERA_STUB := true
 
-# inherit from common msm8660 device
--include device/htc/msm8660-common/BoardConfigCommon.mk
+# Call headers from msm-3.0: needed to build libs in hardware/qcom/display
+TARGET_SPECIFIC_HEADER_PATH := device/htc/shooter/include
 
 # inherit from the proprietary version
 -include vendor/htc/shooter/BoardConfigVendor.mk
 
+# MSM8860
 TARGET_BOOTLOADER_BOARD_NAME := shooter
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := msm8660
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_NO_RADIOIMAGE := true
 
-# TARGET_SPECIFIC_HEADER_PATH := device/htc/shooter/include
-TARGET_SPECIFIC_HEADER_PATH := device/htc/msm8660-common/include
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60 -DQCOM_HARDWARE -DNO_QCOM_MVS
+
+# Scorpion optimizations
+TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
+TARGET_USE_SCORPION_PLD_SET := true
+TARGET_SCORPION_BIONIC_PLDOFFS := 6
+TARGET_SCORPION_BIONIC_PLDSIZE := 128
+
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+BOARD_USES_QCOM_LIBS := true
+
+######################################################
+#  These are hardware settings for the HTC Evo V 4G  #
+#          				             #
+######################################################
+
+# 3D Hardware Support
+TARGET_HARDWARE_3D := true
+TARGET_SLIMER_CAM := true
+
+# Audio
+COMMON_GLOBAL_CFLAGS += -DWITH_QCOM_LPA
+TARGET_USES_QCOM_LPA := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# Camera
+BOARD_HAVE_HTC_FFC := true
+COMMON_GLOBAL_CFLAGS += -DNO_UPDATE_PREVIEW
+COMMON_GLOBAL_CFLAGS += -DHTC_CAMERA
+
+## CDMA-Specific Features
+TARGET_PROVIDES_LIBRIL := vendor/htc/shooter/proprietary/libril.so
+
+# Graphics
+USE_OPENGL_RENDERER := true
+TARGET_HAVE_BYPASS := false
+TARGET_USES_C2D_COMPOSITION := true
+TARGET_USES_OVERLAY := true
+TARGET_QCOM_HDMI_OUT := true
+TARGET_QCOM_HDMI_RESOLUTION_AUTO := true
+BOARD_EGL_CFG := device/htc/shooter/configs/egl.cfg
 
 # Gps
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := shooter
 
-TARGET_HARDWARE_3D := true
-TARGET_SLIMER_CAM := true
+# USB
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun0/file
 
-TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
-TARGET_USE_SCORPION_PLD_SET := true
-# Set to 9 for 8650A
-TARGET_SCORPION_BIONIC_PLDOFFS := 6
-TARGET_SCORPION_BIONIC_PLDSIZE := 128
+# Webkit
+ENABLE_WEBGL := true
+TARGET_FORCE_CPU_UPLOAD := true
+DYNAMIC_SHARED_LIBV8SO := true
 
-# BOARD_HAVE_SQN_WIMAX := true
-BOARD_USES_QCOM_LIBS := true
+# Wifi related defines
+WIFI_BAND                        := 802_11_ABG
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := WEXT
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
+BOARD_HOSTAPD_DRIVER             := WEXT
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_wext
+BOARD_WLAN_DEVICE                := bcm4329
+WIFI_DRIVER_FW_PATH_STA          := "/vendor/firmware/fw_bcm4329.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcm4329_apsta.bin"
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcm4329/parameters/firmware_path"
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/bcm4329.ko"
+WIFI_DRIVER_MODULE_NAME          := "bcm4329"
+WIFI_DRIVER_MODULE_ARG           := "firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/proc/calibration iface_name=wlan"
+BOARD_WLAN_DEVICE_REV            := bcm4329
 
-# cat /proc/emmc
+# WiMax
+BOARD_HAVE_SQN_WIMAX := true
+
+##################################################################
+#  These are specific File System settings for the HTC Evo V 4G  #
+#              DO NOT MAKE EDITS BELOW THIS AREA         	 #
+##################################################################
+
+# Partition information grabbed through adb shell
+# $ cat /proc/emmc 
 #dev:        size     erasesize name
 #mmcblk0p35: 000ffa00 00000200 "misc"
 #mmcblk0p22: 00fffc00 00000200 "recovery"
@@ -47,6 +157,9 @@ BOARD_USES_QCOM_LIBS := true
 #mmcblk0p8: 00c00200 00000200 "wimax"
 #mmcblk0p34: 007ffa00 00000200 "udata_wimax"
 
+# Filesystem
+BOARD_VOLD_MAX_PARTITIONS := 36
+
 BOARD_KERNEL_CMDLINE := console=ttyHSL0 androidboot.hardware=shooter no_console_suspend=1
 BOARD_KERNEL_BASE := 0x48000000
 BOARD_KERNEL_PAGE_SIZE := 2048
@@ -60,6 +173,7 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/tools
 
 # Filesystem
+BOARD_VOLD_MAX_PARTITIONS := 36
 BOARD_SYSTEM_DEVICE := /dev/block/mmcblk0p23
 BOARD_SYSTEM_FILESYSTEM := ext4
 BOARD_DATA_DEVICE := /dev/block/mmcblk0p24
@@ -78,3 +192,6 @@ TARGET_PREBUILT_KERNEL := device/htc/shooter/prebuilt/root/kernel
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_HAS_NO_SELECT_BUTTON := 1
+
+# USB
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun0/file
--- shooter.mk
+++ shooter.mk
@@ -1,6 +1,4 @@
-#
-# Copyright (C) 2011 The Evervolv Project
-# Copyright (C) 2012 The Playground Project
+# Copyright (C) 2012 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,6 +16,87 @@
 # Setup device specific product configuration.
 #
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/base/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+    frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+
+# QCOM Display
+PRODUCT_PACKAGES += \
+    copybit.msm8660 \
+    gralloc.msm8660 \
+    hwcomposer.msm8660 \
+    libgenlock \
+    libmemalloc \
+    liboverlay \
+    libQcomUI \
+    libtilerenderer
+
+# Audio
+PRODUCT_PACKAGES += \
+    libaudioutils
+
+# Omx
+PRODUCT_PACKAGES += \
+    libdivxdrmdecrypt \
+    libmm-omxcore \
+    libOmxCore \
+    libOmxVdec \
+    libOmxVenc \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libstagefrighthw
+
+# HDMI
+PRODUCT_PACKAGES += \
+    hdmid
+
+# Hardware
+PRODUCT_PACKAGES += \
+    gps.shooter \
+    lights.shooter
+
+# Misc
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory \
+    Torch
+
+# Live Wallpapers
+PRODUCT_PACKAGES += \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+    librs_jni
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    setup_fs
+
+# for bugmailer
+ifneq ($(TARGET_BUILD_VARIANT),user)
+    PRODUCT_PACKAGES += send_bug
+    PRODUCT_COPY_FILES += \
+        system/extras/bugmailer/bugmailer.sh:system/bin/bugmailer.sh \
+        system/extras/bugmailer/send_bug:system/bin/send_bug
+endif
+
+
+
 ## (2) Also get non-open-source GSM-specific aspects if available
 $(call inherit-product-if-exists, vendor/htc/shooter/shooter-vendor.mk)
 
@@ -30,14 +109,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     device/htc/shooter/prebuilt/root/init.shooter.rc:root/init.shooter.rc \
+    device/htc/shooter/prebuilt/root/init.shooter.usb.rc:root/init.shooter.usb.rc \
     device/htc/shooter/prebuilt/root/ueventd.shooter.rc:root/ueventd.shooter.rc
-
-#Add touchscreen config file
-PRODUCT_COPY_FILES += \
-    device/htc/shooter/prebuilt/system/usr/idc/atmel-touchscreen.idc:system/usr/idc/atmel-touchscreen.idc \
-    device/htc/shooter/prebuilt/system/usr/idc/projector_input.idc:system/usr/idc/projector_input.idc \
-    device/htc/shooter/prebuilt/system/usr/idc/qwerty.idc:system/usr/idc/qwerty.idc \
-    device/htc/shooter/prebuilt/system/usr/idc/qwerty2.idc:system/usr/idc/qwerty2.idc
 
 # media config xml file
 PRODUCT_COPY_FILES += \
@@ -73,15 +146,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 DEVICE_PACKAGE_OVERLAYS += device/htc/shooter/overlay
 
-# Permissions
-PRODUCT_COPY_FILES += \
-    frameworks/base/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
-
-# Misc
-PRODUCT_PACKAGES += \
-    gps.shooter \
-    lights.shooter
-
 # Keylayouts
 PRODUCT_COPY_FILES += \
     device/htc/shooter/prebuilt/system/usr/keychars/Generic.kcm:system/usr/keychars/Generic.kcm \
@@ -94,18 +158,20 @@ PRODUCT_COPY_FILES += \
     device/htc/shooter/prebuilt/system/usr/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
     device/htc/shooter/prebuilt/system/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
     device/htc/shooter/prebuilt/system/usr/keylayout/shooter-keypad.kl:system/usr/keylayout/shooter-keypad.kl \
-    device/htc/shooter/prebuilt/system/usr/keylayout/Vendor_05ac_Product_0239.kl:system/usr/keylayout/Vendor_05ac_Product_0239.kl \
-    device/htc/shooter/prebuilt/system/usr/keylayout/Vendor_22b8_Product_093d.kl:system/usr/keylayout/Vendor_22b8_Product_093d.kl \
-    device/htc/shooter/prebuilt/system/usr/keylayout/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_028e.kl \
-    device/htc/shooter/prebuilt/system/usr/keylayout/Vendor_046d_Product_c216.kl:system/usr/keylayout/Vendor_046d_Product_c216.kl \
-    device/htc/shooter/prebuilt/system/usr/keylayout/Vendor_046d_Product_c294.kl:system/usr/keylayout/Vendor_046d_Product_c294.kl \
-    device/htc/shooter/prebuilt/system/usr/keylayout/Vendor_046d_Product_c299.kl:system/usr/keylayout/Vendor_046d_Product_c299.kl \
-    device/htc/shooter/prebuilt/system/usr/keylayout/Vendor_046d_Product_c532.kl:system/usr/keylayout/Vendor_046d_Product_c532.kl \
-    device/htc/shooter/prebuilt/system/usr/keylayout/Vendor_054c_Product_0268.kl:system/usr/keylayout/Vendor_054c_Product_0268.kl
+
+#    device/htc/shooter/prebuilt/system/usr/keylayout/Vendor_05ac_Product_0239.kl:system/usr/keylayout/Vendor_05ac_Product_0239.kl \
+#    device/htc/shooter/prebuilt/system/usr/keylayout/Vendor_22b8_Product_093d.kl:system/usr/keylayout/Vendor_22b8_Product_093d.kl \
+#    device/htc/shooter/prebuilt/system/usr/keylayout/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_028e.kl \
+#    device/htc/shooter/prebuilt/system/usr/keylayout/Vendor_046d_Product_c216.kl:system/usr/keylayout/Vendor_046d_Product_c216.kl \
+#    device/htc/shooter/prebuilt/system/usr/keylayout/Vendor_046d_Product_c294.kl:system/usr/keylayout/Vendor_046d_Product_c294.kl \
+#    device/htc/shooter/prebuilt/system/usr/keylayout/Vendor_046d_Product_c299.kl:system/usr/keylayout/Vendor_046d_Product_c299.kl \
+#    device/htc/shooter/prebuilt/system/usr/keylayout/Vendor_046d_Product_c532.kl:system/usr/keylayout/Vendor_046d_Product_c532.kl \
+#    device/htc/shooter/prebuilt/system/usr/keylayout/Vendor_054c_Product_0268.kl:system/usr/keylayout/Vendor_054c_Product_0268.kl
 
 # Firmware
 PRODUCT_COPY_FILES += \
     device/htc/shooter/prebuilt/system/etc/firmware/BCM4329B1_002.002.023.0589.0632.hcd:/system/etc/firmware/BCM4329B1_002.002.023.0589.0632.hcd \
+    device/htc/shooter/prebuilt/system/etc/firmware/bcm4329.hcd:system/vendor/firmware/bcm4329.hcd \
     device/htc/shooter/prebuilt/system/etc/firmware/default_bak.acdb:/system/etc/firmware/default_bak.acdb \
     device/htc/shooter/prebuilt/system/etc/firmware/fw_bcm4329.bin:/system/etc/firmware/fw_bcm4329.bin \
     device/htc/shooter/prebuilt/system/etc/firmware/fw_bcm4329_apsta.bin:/system/etc/firmware/fw_bcm4329_apsta.bin \
@@ -113,12 +179,25 @@ PRODUCT_COPY_FILES += \
     device/htc/shooter/prebuilt/system/etc/firmware/leia_pm4_470.fw:/system/etc/firmware/leia_pm4_470.fw \
     device/htc/shooter/prebuilt/system/etc/firmware/vidc_1080p.fw:/system/etc/firmware/vidc_1080p.fw
 
+#Touchscreen config file
+PRODUCT_COPY_FILES += \
+    device/htc/shooter/prebuilt/system/usr/idc/atmel-touchscreen.idc:system/usr/idc/atmel-touchscreen.idc \
+    device/htc/shooter/prebuilt/system/usr/idc/projector_input.idc:system/usr/idc/projector_input.idc \
+    device/htc/shooter/prebuilt/system/usr/idc/qwerty.idc:system/usr/idc/qwerty.idc \
+    device/htc/shooter/prebuilt/system/usr/idc/qwerty2.idc:system/usr/idc/qwerty2.idc \
+
+#Config file
+PRODUCT_COPY_FILES += \
+    device/htc/shooter/configs/thermald.conf:system/etc/thermald.conf \
+    device/htc/shooter/configs/audio_policy.conf:system/etc/audio_policy.conf \
+    device/htc/shooter/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    device/htc/shooter/configs/apns-conf.xml:system/etc/apns-conf.xml
+
+
 PRODUCT_COPY_FILES += \
     device/htc/shooter/vold.fstab:system/etc/vold.fstab \
 
 TARGET_PREBUILT_KERNEL := device/htc/shooter/prebuilt/root/kernel
-
-ifneq ($(TARGET_PREBUILT_KERNEL),)
 
 # Local Kernel
 LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
@@ -126,12 +205,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
 # Kernel Modules
-PRODUCT_COPY_FILES += $(shell \
-    find device/htc/shooter/prebuilt/system/lib/modules -name '*.ko' \
-    | sed -r 's/^\/?(.*\/)([^/ ]+)$$/\1\2:system\/lib\/modules\/\2/' \
-    | tr '\n' ' ')
-
-endif
+PRODUCT_COPY_FILES += \
+    device/htc/shooter/prebuilt/system/lib/modules/bcm4329.ko:system/lib/modules/bcm4329.ko \
+    device/htc/shooter/prebuilt/system/lib/modules/sequans_sdio.ko:system/lib/modules/sequans_sdio.ko \
+    device/htc/shooter/prebuilt/system/lib/modules/wimaxdbg.ko:system/lib/modules/wimaxdbg.ko \
+    device/htc/shooter/prebuilt/system/lib/modules/wimaxuart.ko:system/lib/modules/wimaxuart.ko
 
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
@@ -142,8 +220,6 @@ PRODUCT_COPY_FILES += \
     device/htc/shooter/prebuilt/system/etc/vold.fstab:system/etc/vold.fstab
 
 # common msm8660 configs
-$(call inherit-product, device/htc/msm8660-common/msm8660.mk)
-
 $(call inherit-product, frameworks/base/build/phone-hdpi-512-dalvik-heap.mk)
 
 $(call inherit-product, build/target/product/full_base.mk)
@@ -153,4 +229,18 @@ PRODUCT_DEVICE := shooter
 PRODUCT_BRAND := virgin_mobile
 PRODUCT_MODEL := PG86100
 PRODUCT_MANUFACTURER := HTC
+
+# Common Qualcomm scripts
+PRODUCT_COPY_FILES += \
+    device/htc/shooter/prebuilt/init.qcom.efs.sync.sh:system/etc/init.qcom.efs.sync.sh \
+    device/htc/shooter/prebuilt/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh 
+
+# We have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
 
